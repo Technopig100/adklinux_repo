5ab3aecf6a454e2bf6a4c4d0d60899bd12d5825b
jacolinux <technopig@e.email>
2022-02-13 01:50:45 -0500

    update

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm adk_repo*
+
+echo "repo-add"
+#repo-add -s -n -R adk_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R adk_repo.db.tar.gz *.pkg.tar.zst
+sleep 5
+#cp adk_repo_testing.db.tar.gz adk_repo_testing.db
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
