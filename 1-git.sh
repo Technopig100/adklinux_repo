702c945fe05266ef15efa883217ffc3617f0aa35
Technopig100 <technopig10@gmail.com>
2022-04-21 14:06:41 -0400

    update

--- 1-git.sh
+++ 1-git.sh
@@ -23,13 +23,13 @@ project=$(basename `pwd`)
 echo "-----------------------------------------------------------------------------"
 echo "this is project https://github.com/Technopig100/"$project
 echo "-----------------------------------------------------------------------------"
-git config --global pull.rebase false
-git config --global user.name "Technopig100"
-git config --global user.email "technopig10@gmail.com"
+git config --local pull.rebase false
+git config --local user.name "Technopig100"
+git config --local user.email "technopig10@gmail.com"
 sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=32000'
-git config --global push.default simple
+git config --local credential.helper cache
+git config --local credential.helper 'cache --timeout=32000'
+git config --local push.default simple
 
 git remote set-url origin git@github.com:Technopig100/$project
 
